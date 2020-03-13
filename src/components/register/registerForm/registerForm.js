20a9ecc209f13e5c452c01fc36e923d2fcfde41a
Gladyshev Roman <romagladyshev@gmail.com>
2020-03-13 12:19:50 +0600

    Configure register form

--- src/components/register/registerForm/registerForm.js
+++ src/components/register/registerForm/registerForm.js
@@ -4,62 +4,67 @@ import { Form, Field } from "react-final-form";
 const RegisterForm = props => {
   return (
     <Form
-      onSubmit={() => props.onSubmit()}
+      onSubmit={values => console.log(values)}
       validate={values => {
         const errors = {};
 
         if (values.password !== values.confirmPassword)
-          errors.confirmPassword = "Password are not equal";
+          errors.confirmPassword = "Passwords are not equal";
+
+        return errors;
       }}
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit} className="modal-signUp__form">
-          <div className="modal-signUp__input-wrap input-wrap">
-            <Field name="email">
-              {({ input, meta }) => (
-                <>
-                  <i className="fa fa-user input-user" aria-hidden="true"></i>
-                  <input
-                    type="email"
-                    className="modal-signIn__input modal-input"
-                    name="email"
-                    placeholder="Email"
-                    required
-                  />
-                </>
-              )}
-            </Field>
-          </div>
-          <div className="modal-signUp__input-wrap input-wrap">
-            <Field name="password">
-              {({ input, meta }) => (
-                <>
-                  <i className="fa fa-lock input-lock" aria-hidden="true"></i>
-                  <input
-                    type="password"
-                    className="modal-signIn__input modal-input"
-                    name="password"
-                    placeholder="Password"
-                    required
-                  />
-                </>
-              )}
-            </Field>
-          </div>
-          <div className="modal-signUp__input-wrap input-wrap">
-            <Field name="confirmPassword">
-              {({ input, meta }) => (
-                <>
+          <Field name="email">
+            {({ input, meta }) => (
+              <div className="modal-signUp__input-wrap input-wrap">
+                <i className="fa fa-user input-user" aria-hidden="true"></i>
+                <input
+                  {...input}
+                  type="email"
+                  className="modal-signIn__input modal-input"
+                  name="email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+            )}
+          </Field>
+          <Field name="password">
+            {({ input, meta }) => (
+              <div className="modal-signUp__input-wrap input-wrap">
+                <i className="fa fa-lock input-lock" aria-hidden="true"></i>
+                <input
+                  {...input}
+                  type="password"
+                  className="modal-signIn__input modal-input"
+                  name="password"
+                  placeholder="Password"
+                  minLength={8}
+                  required
+                />
+              </div>
+            )}
+          </Field>
+          <Field name="confirmPassword">
+            {({ input, meta }) => (
+              <>
+                <div className="modal-signUp__input-wrap input-wrap">
                   <i className="fa fa-check input-check" aria-hidden="true"></i>
                   <input
+                    {...input}
                     type="password"
                     className="modal-signIn__input modal-input"
                     placeholder="Confirm Password"
                     required
                   />
-                </>
-              )}
-            </Field>
-          </div>
+                </div>
+                {meta.touched && meta.error && (
+                  <p style={{ color: "red" }}>{meta.error}</p>
+                )}
+              </>
+            )}
+          </Field>
           <p className="modal-signUp__desc">
             By clicking Sign Up, you agree to our
             <a href="#" className="page-link">
